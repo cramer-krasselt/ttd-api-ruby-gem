2129ef1eab21ec54157431f3a81bffa08cf358e6
Mo Omer <momer@c-k.com>
2016-11-29 09:45:11 -0600

    Add list to base

--- lib/crimsonhexagon/base.rb
+++ lib/crimsonhexagon/base.rb
@@ -1,5 +1,12 @@
 module CrimsonHexagon
   class Base < API
     include CrimsonHexagon::Monitor::Common
+
+    def list(team = nil)
+      options = {}
+      options.merge(team: team) if team
+      response = get("monitor/list", options)
+      response.monitors
+    end
   end
 end
