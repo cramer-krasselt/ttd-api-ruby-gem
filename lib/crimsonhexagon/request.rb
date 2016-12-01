f10eeae850eace725f5fd61da2241ed3c5cf3294
Mo Omer <momer@c-k.com>
2016-12-01 10:22:45 -0600

    Path unformatted by default

--- lib/crimsonhexagon/request.rb
+++ lib/crimsonhexagon/request.rb
@@ -6,29 +6,29 @@ module CrimsonHexagon
   # Defines HTTP request methods
   module Request
     # Perform an HTTP GET request
-    def get(path, options={}, signature=false, raw=false, unformatted=false, no_response_wrapper=no_response_wrapper(), signed=sign_requests)
+    def get(path, options={}, signature=false, raw=false, unformatted=true, no_response_wrapper=no_response_wrapper(), signed=sign_requests)
       request(:get, path, options, signature, raw, unformatted, no_response_wrapper, signed)
     end
 
     # Perform an HTTP POST request
-    def post(path, options={}, signature=false, raw=false, unformatted=false, no_response_wrapper=no_response_wrapper(), signed=sign_requests)
+    def post(path, options={}, signature=false, raw=false, unformatted=true, no_response_wrapper=no_response_wrapper(), signed=sign_requests)
       request(:post, path, options, signature, raw, unformatted, no_response_wrapper, signed)
     end
 
     # Perform an HTTP PUT request
-    def put(path, options={},  signature=false, raw=false, unformatted=false, no_response_wrapper=no_response_wrapper(), signed=sign_requests)
+    def put(path, options={},  signature=false, raw=false, unformatted=true, no_response_wrapper=no_response_wrapper(), signed=sign_requests)
       request(:put, path, options, signature, raw, unformatted, no_response_wrapper, signed)
     end
 
     # Perform an HTTP DELETE request
-    def delete(path, options={}, signature=false, raw=false, unformatted=false, no_response_wrapper=no_response_wrapper(), signed=sign_requests)
+    def delete(path, options={}, signature=false, raw=false, unformatted=true, no_response_wrapper=no_response_wrapper(), signed=sign_requests)
       request(:delete, path, options, signature, raw, unformatted, no_response_wrapper, signed)
     end
 
     private
 
     # Perform an HTTP request
-    def request(method, path, options, signature=false, raw=false, unformatted=false, no_response_wrapper=false, signed=sign_requests)
+    def request(method, path, options, signature=false, raw=false, unformatted=true, no_response_wrapper=false, signed=sign_requests)
       response = connection(raw).send(method) do |request|
         path = formatted_path(path) unless unformatted
 
